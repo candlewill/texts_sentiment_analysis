8d77611dec60f21eacfaed12a0a0bfcedd100689
candlewill <yunchaohe@gmail.com>
2015-05-03 20:32:17 +0800

    增加SVM分类器，不过目前仍有问题

--- parameters.py
+++ parameters.py
@@ -1,8 +1,9 @@
 __author__ = 'NLP-PC'
 parameters = {'min_df': 1/2000,
               'ngram_range': (1, 3),
-              'test_data_size': 20000,
+              'test_data_size': 10000,
               'max_df': 0.8,
               'TF_binary': True,
               'norm': 'l1',
-              'sublinear_tf': True}
+              'sublinear_tf': True,
+              'classifier': 'svm'}
--- simple_main.py
+++ simple_main.py
@@ -32,7 +32,7 @@ for train, test in cv:
     X_test, Y_test = np.array(X_test), np.array(Y_test)
 
     from vectorizer_estimator import StatisticVectorizer
-    from sklearn.pipeline import Pipeline, FeatureUnion
+    from sklearn.pipeline import FeatureUnion
     statistic_vec=StatisticVectorizer()
     combined_features =FeatureUnion([('ngrams',vectorizer),('statistic_vec',statistic_vec)])
 
@@ -42,44 +42,55 @@ for train, test in cv:
 
     # clf = pickle.load(open("./acc_tmp/clf_all_data_noclustering.p", "rb"))
 
-    # from sklearn import svm
-    # clf = svm.SVC()
-    clf = MultinomialNB()
-    # pipeline = Pipeline([("features", combined_features), ("clf", clf)])
-    clf.fit(trian_vec, Y_train)
-    pickle.dump(trian_vec, open("./debug/trian_vec.p", "wb"))
-    pickle.dump(clf, open("./acc_tmp/clf.p", "wb"))
-    print(trian_vec.shape, len(X_test))
-    print(test_vec.shape, len(Y_test))
-    test_score = clf.score(test_vec, Y_test)
+    if parameters['classifier']=='svm':
+        from sklearn import svm
+        clf = svm.SVC()
+        clf.fit(trian_vec.toarray(), Y_train)
+        pickle.dump(trian_vec, open("./debug/trian_vec.p", "wb"))
+        pickle.dump(clf, open("./acc_tmp/clf.p", "wb"))
+        true_labels=Y_test
+        predict_labels=np.array(clf.predict(test_vec.toarray()))
+        from sklearn.metrics import precision_recall_fscore_support
+        precision,recall,fbeta_score,support=precision_recall_fscore_support(true_labels, predict_labels, average='binary')
+        print('精确度(Precision):%.3f\n召回率：%.3f\nF值: %.3f'%(precision,recall,fbeta_score))
+    else:
+        clf = MultinomialNB()
+        clf.fit(trian_vec, Y_train)
+        pickle.dump(trian_vec, open("./debug/trian_vec.p", "wb"))
+        pickle.dump(clf, open("./acc_tmp/clf.p", "wb"))
+        print(trian_vec.shape, len(X_test))
+        print(test_vec.shape, len(Y_test))
+        test_score = clf.score(test_vec, Y_test)
 
-    scores = []
-    scores.append(test_score)
-    proba = clf.predict_proba(test_vec)
-    from sklearn.metrics import precision_recall_curve, auc
+        scores = []
+        scores.append(test_score)
+        proba = clf.predict_proba(test_vec)
+        from sklearn.metrics import precision_recall_curve, auc
 
-    precision, recall, pr_thresholds = precision_recall_curve(Y_test, proba[:, 1])
-    # AUC
-    aera_uc = auc(recall, precision)
-    pr_scores = []
-    pr_scores.append(aera_uc)
+        precision, recall, pr_thresholds = precision_recall_curve(Y_test, proba[:, 1])
+        # AUC
+        aera_uc = auc(recall, precision)
+        pr_scores = []
+        pr_scores.append(aera_uc)
 
-    # F1_score
-    from sklearn.metrics import f1_score
-    f1 = []
-    f1.append(f1_score(Y_test, clf.predict(test_vec), average='macro'))
+        # F1_score
+        from sklearn.metrics import f1_score
+        f1 = []
+        f1.append(f1_score(Y_test, clf.predict(test_vec), average='macro'))
 
-    summary = (np.mean(scores), np.mean(pr_scores), np.mean(f1))
-    # Area Under Curve （曲线下面的面积）
-    print('正确率(Accuracy)：%.3f\nP/R AUC值：%.3f\nF值(Macro-F score)：%.3f' % (summary))
+        summary = (np.mean(scores), np.mean(pr_scores), np.mean(f1))
+        # Area Under Curve （曲线下面的面积）
+        print('正确率(Accuracy)：%.3f\nP/R AUC值：%.3f\nF值(Macro-F score)：%.3f' % (summary))
+
+        # 画图
+        pl.clf()
+        pl.plot(recall, precision, label='Precision-Recall curve')
+        pl.xlabel('Recall')
+        pl.ylabel('Precision')
+        pl.ylim([0.0, 1.05])
+        pl.xlim([0.0, 1.0])
+        pl.title('Precision-Recall Curve (AUC=%0.3f)' % aera_uc)
+        pl.legend(loc="lower left")
+        pl.show()
+
     print(sorted(list(parameters.items())))
-    # 画图
-    pl.clf()
-    pl.plot(recall, precision, label='Precision-Recall curve')
-    pl.xlabel('Recall')
-    pl.ylabel('Precision')
-    pl.ylim([0.0, 1.05])
-    pl.xlim([0.0, 1.0])
-    pl.title('Precision-Recall Curve (AUC=%0.3f)' % aera_uc)
-    pl.legend(loc="lower left")
-    pl.show()
